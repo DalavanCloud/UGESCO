1a00df7bd80354e219f18bab8bf041d6bb996cea
Ettore Rizza <ettorerizza@gmail.com>
2018-01-06 12:58:07 +0100

    added get_wikidata

--- functions/get_wikidata.py
+++ functions/get_wikidata.py
@@ -8,42 +8,64 @@ Created on Fri Jan  5 12:35:54 2018
 import requests
 import pandas as pd
 
-base_url = "https://tools.wmflabs.org/openrefine-wikidata/en/api"
+def get_wikidata(query, type_id, prop_id, prop_value):
+    """ Use the Antonin's API to return the best match on Wikidata based on type and a property.
+    Best match = exact match, or higher score + lesser qid magnitude
+    The result is a tuple (id_magnitude, main_type, match, name, qid, score)
+    Example : get_wikidata('Binche', 'Q618123', 'P17', 'Q31')
+    Result : (95121, 'municipality of Belgium', False, 'Binche', 'Q95121', 100.0)
+    """
+    base_url = "https://tools.wmflabs.org/openrefine-wikidata/en/api"
 
-query = {"query" : """{"query":"Paris",
+    query = {"query": """{"query":"%s",
                       "limit":6,
-                      "type" : "Q618123",
+                      "type" : "%s",
                       "properties" : [
-                         {"pid":"P17","v":"Q142"}
+                         {"pid":"%s","v":"%s"}
                          ]
-                         }"""}
+                         }""" % (query, type_id, prop_id, prop_value)}
 
-r = requests.get(base_url, params = query)
+    r = requests.get(base_url, params=query)
 
-#print(r.url)
+    # print(r.url)
 
-json_result = r.json()
+    json_result = r.json()
+    
+    # print(json_result)
 
-#print(json_result)
+    try:
+        qid = [d['id'] for d in json_result['result']]
+        id_magnitude = [int(d['id'].replace("Q", ''))
+                        for d in json_result['result']]
+        name = [d['name'] for d in json_result['result']]
+        score = [d['score'] for d in json_result['result']]
+        match = [d['match'] for d in json_result['result']]
+        main_type = [d['type'][0]['name'] for d in json_result['result']]
 
-qid = [d['id'] for d in json_result['result']]
-id_magnitude = [int(d['id'].replace("Q", '')) for d in json_result['result']]
-name = [d['name'] for d in json_result['result']]
-score = [d['score'] for d in json_result['result']]
-match = [d['match'] for d in json_result['result']]
+        df = pd.DataFrame({'qid': qid,
+                           'name': name,
+                           'id_magnitude': id_magnitude,
+                           'score': score,
+                           'match': match,
+                           'main_type': main_type
+                           })
 
-df = pd.DataFrame({  'qid': qid,
-                     'name': name,
-                     'id_magnitude': id_magnitude,
-                     'score': score,
-                     'match':match
-                  })
-     
-#order by score then by inverse qid magnitude
-df.sort_values(['score', 'id_magnitude'], ascending=[False, True], inplace=True)
+        # order by score then by inverse qid magnitude
+        df.sort_values(['score', 'id_magnitude'], ascending=[
+                       False, True], inplace=True)
+
+        # select the best match
+        match = df[df['match'] == True].values
+
+        if match.size > 0:
+            best_match = match
+        else:
+            best_match = tuple(map(tuple, df.iloc[[0]].values))[0]
+
+        return best_match
+        
+    except IndexError:
+        return "No match"
 
 
-
-
-
-
+get_wikidata('Binche', 'Q618123', 'P17', 'Q31')
--- ugesco.py
+++ ugesco.py
@@ -6,6 +6,65 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_wikidata(query, type_id, prop_id, prop_value):
+    """ Use the Antonin's API to return the best match on Wikidata based on type and property.
+    Best match = match, or higher score + lesser qid magnitude
+    The result is a tuple (id_magnitude, main_type, match, name, qid, score)
+    Example : get_wikidata('Binche', 'Q618123', 'P17', 'Q31')
+    Result : (95121, 'municipality of Belgium', False, 'Binche', 'Q95121', 100.0)
+    """
+    base_url = "https://tools.wmflabs.org/openrefine-wikidata/en/api"
+
+    query = {"query": """{"query":"%s",
+                      "limit":6,
+                      "type" : "%s",
+                      "properties" : [
+                         {"pid":"%s","v":"%s"}
+                         ]
+                         }""" % (query, type_id, prop_id, prop_value)}
+
+    r = requests.get(base_url, params=query)
+
+    # print(r.url)
+
+    json_result = r.json()
+
+    try:
+        # print(json_result)
+
+        qid = [d['id'] for d in json_result['result']]
+        id_magnitude = [int(d['id'].replace("Q", ''))
+                        for d in json_result['result']]
+        name = [d['name'] for d in json_result['result']]
+        score = [d['score'] for d in json_result['result']]
+        match = [d['match'] for d in json_result['result']]
+        main_type = [d['type'][0]['name'] for d in json_result['result']]
+
+        df = pd.DataFrame({'qid': qid,
+                           'name': name,
+                           'id_magnitude': id_magnitude,
+                           'score': score,
+                           'match': match,
+                           'main_type': main_type
+                           })
+
+        # order by score then by inverse qid magnitude
+        df.sort_values(['score', 'id_magnitude'], ascending=[
+                       False, True], inplace=True)
+
+        # select the best match
+        match = df[df['match'] == True].values
+
+        if match.size > 0:
+            best_match = match
+        else:
+            best_match = tuple(map(tuple, df.iloc[[0]].values))[0]
+
+        return best_match
+    except IndexError:
+        return "No match"
+
+
 def getClass(item):
     """
     get the class(es) of a Wikidata item
@@ -28,6 +87,7 @@ def getClass(item):
     soup = BeautifulSoup(r.text, "lxml")
 
     return [x.text for x in soup.find_all("uri")]
+
 
 def getLCS(array):
     """
@@ -54,8 +114,6 @@ def getLCS(array):
     soup = BeautifulSoup(r.text, "lxml")
 
     return [x.text for x in soup.find_all("literal")]
-
-
 
 
 class MyEncoder(json.JSONEncoder):

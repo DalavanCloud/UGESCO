c2375bc30db00496caf9d02f3dc606166ec52864
Ettore Rizza <ettorerizza@gmail.com>
2018-01-23 14:51:27 +0100

    Modified getLCS

--- functions/getLCS.py
+++ functions/getLCS.py
@@ -1,35 +1,53 @@
 # -*- coding: utf-8 -*-
 import requests
+import json
 from bs4 import BeautifulSoup
 
-array = ["Q32815", "Q34627"] #mosque and synagogue
+
+def classToQid(array):
+    """ get Wikidata qid from computer vision classes"""
+    qid_array = []
+
+    with open(r"classes_places.json", "r") as f:
+        classes = json.load(f)
+
+    for i in array:
+        i = i.split('/')[0].lower().strip()
+        qid_array.append("wd:" + classes[i])
+    return ", ".join(qid_array)
+    
 
 
 def getLCS(array):
     """
-    get the least common subsumer between the two
-    first Wikidata items in a list
+    get the Least common subsumer between several Wikidata qids in a list
     """
     query = {"query": """
-    SELECT ?classe ?classeLabel WHERE { 
-    wd:%s wdt:P279* ?classe . 
-    wd:%s wdt:P279* ?classe . 
-    filter not exists { ?otherClass wdt:P279 ?classe ; ^wdt:P279* wd:%s, wd:%s . } 
+    SELECT ?lcs ?lcsLabel WHERE {
 
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } 
-    }
-    """ % (array[0], array[1], array[0], array[1])
+    ?lcs ^wdt:P279* %s .
+    filter not exists {
+    ?sublcs ^wdt:P279* %s ;
+          wdt:P279 ?lcs .
+      }
+
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } }""" % (array, array)
     }
 
     url = "https://query.wikidata.org/sparql"
 
     r = requests.get(url, params=query)
 
-    # print(r.text)
-
     soup = BeautifulSoup(r.text, "lxml")
 
     return [x.text for x in soup.find_all("literal")]
 
 
-print(getLCS(array))
+if __name__ == '__main__':
+    
+    array = ["synagogue/indoor", "mosque/outdoor",
+         "church", "palace"]  
+
+    list_of_qids = classToQid(array)
+
+    print(getLCS(list_of_qids))

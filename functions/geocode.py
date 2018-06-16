601fe21a744d04c2cdf5515cd0a0be932640aa09
Ettore Rizza <ettorerizza@gmail.com>
2018-06-16 12:08:23 +0200

    modified geocode

--- functions/geocode.py
+++ functions/geocode.py
@@ -1,16 +1,22 @@
-# https://www.programcreek.com/python/example/108180/geopy.geocoders.Nominatim
+# examples : https://www.programcreek.com/python/example/108180/geopy.geocoders.Nominatim
 
 from geopy.geocoders import Nominatim
 
 
+def geocode(value, country, one=True):
+    geolocator = Nominatim(country_bias=country,
+                           user_agent='ettorerizza@gmail.com')
 
-def geocode(value, country):
-    geolocator = Nominatim(country_bias=country)
-    location = geolocator.geocode(value, exactly_one=False)
+    location = geolocator.geocode(value,
+                                  exactly_one=one,
+                                  limit=None,
+                                  addressdetails=True, 
+                                  language="FR",
+                                  geometry="KML")
     liste = []
     try:
-        if len(location) == 1:
-            return location[0].raw
+        if len(location) == 2:
+            return location.raw
         else:
             for i in location:
                 liste.append(i.raw)
@@ -22,5 +28,4 @@ def geocode(value, country):
 
 if __name__ == '__main__':
 
-    print(geocode("rue Rogier  Namur", "BE"))
-
+    print(geocode("arc de triomphe", "FR"))

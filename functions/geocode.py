008cbd8559e5286b71b7fe3db41b4436273bce2a
Ettore Rizza <ettorerizza@gmail.com>
2018-06-16 02:15:41 +0200

    changed geocode

--- functions/geocode.py
+++ functions/geocode.py
@@ -1,7 +1,7 @@
 # https://www.programcreek.com/python/example/108180/geopy.geocoders.Nominatim
 
 from geopy.geocoders import Nominatim
-import google_streetview.api
+
 
 
 def geocode(value, country):
@@ -22,19 +22,5 @@ def geocode(value, country):
 
 if __name__ == '__main__':
 
-    print(geocode("gare du nord", "BE"))
+    print(geocode("rue Rogier  Namur", "BE"))
 
-    # Import google_streetview for the api module
-
-    # Define parameters for street view api
-    params = [{
-            'size': '600x300',  # max 640x640 pixels
-            'location': 'rue de bruxelles braine-le-comte',
-            'heading': '151.78',
-            'pitch': '-0.76',
-            'key': ''
-        }]
-
-        # Create a results object
-    results = google_streetview.api.results(params)
-    results.download_links('downloads')
